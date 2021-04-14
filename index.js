2c13b695401ed3ede3b519402b8ee4cc1a485da2
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 11:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str){
+  cosole.log(str);
+}
 /* Write your implementation of displayMessage() */
